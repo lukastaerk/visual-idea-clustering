070268ffcc20fbed6ba4da139c7b58907120aa2d
luka.staerk <luka.staerk@campus.tu-berlin.de>
2019-01-29 18:08:06 +0100

    new state export

--- src/utils/downloadState.js
+++ src/utils/downloadState.js
@@ -2,7 +2,7 @@ var FileSaver = require("file-saver");
 
 export const downloadState = state => {
   let date = new Date();
-  var blob = new Blob([JSON.stringify(state, null, 2)], {
+  var blob = new Blob([JSON.stringify(convert(state), null, 2)], {
     type: "application/json;charset=utf-8"
   });
   FileSaver.saveAs(
@@ -15,5 +15,15 @@ export const downloadState = state => {
       ".json"
   );
 };
+
+function convert(state) {
+  var { boardIdeas, clusters } = state;
+  var ideas = boardIdeas.map(idea => ({ ...idea, cluster: [] }));
+  clusters.forEach(c => {
+    ideas = [...ideas, ...c.ideas.map(i => ({ ...i, cluster: [c.id] }))];
+  });
+  clusters = clusters.map(({ id, name, position }) => ({ id, name, position }));
+  return { ideas, clusters };
+}
 
 export default downloadState;
